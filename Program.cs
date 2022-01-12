28ed867148e1ebe1188cf031b9a8eb102ebd415c
1472258 <96775548+1472258@users.noreply.github.com>
2022-01-12 18:57:25 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int[] array = new int[] {1, 42, 4, 452, 70, 69, 80, 741, 82, 91, 92 };
+var str = string.Join(" ", array);
+Console.WriteLine(str);
+ for(int i=0; i<=9; i++) {
+   if(array[i]%2 == 0) {
+    Console.Write("{0} ", array[i]);}
+}
